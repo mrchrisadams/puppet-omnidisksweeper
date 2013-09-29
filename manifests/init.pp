5bbab49c599d13b594bf47187b15bddc6b7981af
Chris Adams <wave@chrisadams.me.uk>
2013-09-29 18:23:25 +0100

    First failing attempt to build a module that asks you to accept a license before installing.

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,4 +1,13 @@
 # This is a placeholder class.
-class template {
-  anchor { 'Hello_World': }
+
+class omnidisksweeper {
+  package { 'omnidisksweeper':
+    # sadly, this needs a different provider, that lets you accept the request
+    # however, to do this, you need a new version of boxen
+      # however you need a new version of homebrew to support the new syntax for nginx
+        # the new version of homebrew causes a "Error: Could not apply complete catalog: Found 1 dependency cycle" error
+          # so this experiment here ends
+    provider   => 'appdmg',
+    source => 'http://downloads2.omnigroup.com/software/MacOSX/10.6/OmniDiskSweeper-1.8.dmg'
+  }
 }
--- spec/classes/omnidisksweeper_spec.rb
+++ spec/classes/omnidisksweeper_spec.rb
@@ -2,8 +2,9 @@ require 'spec_helper'
 # Rename this file to classname_spec.rb
 # Check other boxen modules for examples
 # or read http://rspec-puppet.com/tutorial/
-describe 'template' do
+describe 'omnidisksweeper' do
   it do
-    should contain_anchor('Hello_World')
+    # should you mock an environment?
+    should contain_package('omnidisksweeper')
   end
 end
