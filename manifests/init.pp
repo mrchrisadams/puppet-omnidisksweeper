57dc8cd25b41459615e99dfbc7ac9839981ce388
Chris Adams <wave@chrisadams.me.uk>
2013-09-30 03:43:01 +0100

    WOOHOO finally got a passing spec!
    AND I understand how the fixtures work!

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,7 +1,7 @@
-
+# include omnidisksweeper
 class omnidisksweeper {
   package { 'OmniDiskSweeper':
-    provider   => 'appdmg_eula',
-    source => 'http://downloads2.omnigroup.com/software/MacOSX/10.6/OmniDiskSweeper-1.8.dmg'
+    provider  => 'appdmg_eula',
+    source    => 'http://downloads2.omnigroup.com/software/MacOSX/10.6/OmniDiskSweeper-1.8.dmg'
   }
 }
--- spec/classes/omnidisksweeper_spec.rb
+++ spec/classes/omnidisksweeper_spec.rb
@@ -4,27 +4,6 @@ require 'spec_helper'
 # or read http://rspec-puppet.com/tutorial/
 describe 'omnidisksweeper' do
 
-# We have the class and package setup. Why would this fail?
-# Failures:
-
-#   1) omnidisksweeper
-#      Failure/Error: it { should contain_class('omnidisksweeper') }
-#      Puppet::Error:
-#        Could not find class omnidisksweeper for munster.local on node munster.local
-#      # ./spec/classes/omnidisksweeper_spec.rb:6:in `block (2 levels) in <top (required)>'
-
-#   2) omnidisksweeper
-#      Failure/Error: it { should contain_package('OmniDiskSweeper').with_provider('appdmg_eula') }
-#      Puppet::Error:
-#        Could not find class omnidisksweeper for munster.local on node munster.local
-#      # ./spec/classes/omnidisksweeper_spec.rb:7:in `block (2 levels) in <top (required)>'
-
-#   3) omnidisksweeper
-#      Failure/Error: it { should contain_package('OmniDiskSweeper').with_source('http://downloads2.omnigroup.com/software/MacOSX/10.6/OmniDiskSweeper-1.8.dmg') }
-#      Puppet::Error:
-#        Could not find class omnidisksweeper for munster.local on node munster.local
-#      # ./spec/classes/omnidisksweeper_spec.rb:8:in `block (2 levels) in <top (required)>'
-
   it { should contain_class('omnidisksweeper') }
   it { should contain_package('OmniDiskSweeper').with_provider('appdmg_eula') }
   it { should contain_package('OmniDiskSweeper').with_source('http://downloads2.omnigroup.com/software/MacOSX/10.6/OmniDiskSweeper-1.8.dmg') }
