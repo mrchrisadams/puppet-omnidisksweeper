a254ab528d0dc7dbdf9b986caf2015b7921f6806
Chris Adams <wave@chrisadams.me.uk>
2013-09-30 03:18:01 +0100

    Add missing part explaining why tests are failing

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,13 +1,7 @@
-# This is a placeholder class.
 
 class omnidisksweeper {
-  package { 'omnidisksweeper':
-    # sadly, this needs a different provider, that lets you accept the request
-    # however, to do this, you need a new version of boxen
-      # however you need a new version of homebrew to support the new syntax for nginx
-        # the new version of homebrew causes a "Error: Could not apply complete catalog: Found 1 dependency cycle" error
-          # so this experiment here ends
-    provider   => 'appdmg',
+  package { 'OmniDiskSweeper':
+    provider   => 'appdmg_eula',
     source => 'http://downloads2.omnigroup.com/software/MacOSX/10.6/OmniDiskSweeper-1.8.dmg'
   }
 }
--- spec/classes/omnidisksweeper_spec.rb
+++ spec/classes/omnidisksweeper_spec.rb
@@ -3,8 +3,29 @@ require 'spec_helper'
 # Check other boxen modules for examples
 # or read http://rspec-puppet.com/tutorial/
 describe 'omnidisksweeper' do
-  it do
-    # should you mock an environment?
-    should contain_package('omnidisksweeper')
-  end
+
+# We have the class and package setup. Why would this fail?
+# Failures:
+
+#   1) omnidisksweeper
+#      Failure/Error: it { should contain_class('omnidisksweeper') }
+#      Puppet::Error:
+#        Could not find class omnidisksweeper for munster.local on node munster.local
+#      # ./spec/classes/omnidisksweeper_spec.rb:6:in `block (2 levels) in <top (required)>'
+
+#   2) omnidisksweeper
+#      Failure/Error: it { should contain_package('OmniDiskSweeper').with_provider('appdmg_eula') }
+#      Puppet::Error:
+#        Could not find class omnidisksweeper for munster.local on node munster.local
+#      # ./spec/classes/omnidisksweeper_spec.rb:7:in `block (2 levels) in <top (required)>'
+
+#   3) omnidisksweeper
+#      Failure/Error: it { should contain_package('OmniDiskSweeper').with_source('http://downloads2.omnigroup.com/software/MacOSX/10.6/OmniDiskSweeper-1.8.dmg') }
+#      Puppet::Error:
+#        Could not find class omnidisksweeper for munster.local on node munster.local
+#      # ./spec/classes/omnidisksweeper_spec.rb:8:in `block (2 levels) in <top (required)>'
+
+  it { should contain_class('omnidisksweeper') }
+  it { should contain_package('OmniDiskSweeper').with_provider('appdmg_eula') }
+  it { should contain_package('OmniDiskSweeper').with_source('http://downloads2.omnigroup.com/software/MacOSX/10.6/OmniDiskSweeper-1.8.dmg') }
 end
